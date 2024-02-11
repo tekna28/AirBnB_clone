18075776d4cb5167c9d37548dc1e6caabe35e102
tekna28 <salma.jeghloul@gmail.com>
2024-02-11 10:10:35 -0800

    created class BaseModel and updated unit tests

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class. All the other classes inherits from here."""
+
+    def __init__(self, *_args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        cls_dict = {'__class__': self.__class__.__name__}
+        cls_dict.update({k: v.isoformat()
+                        if isinstance(v, datetime)
+                        else v for k, v in self.__dict__.items()})
+        return cls_dict
