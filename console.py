8e83c692e92ba37729df4e98f4486abc014cb963
tekna28 <salma.jeghloul@gmail.com>
2024-02-11 14:12:55 -0800

    update

--- console.py
+++ console.py
@@ -6,7 +6,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
@@ -33,7 +37,12 @@ class HBNBCommand(cmd.Cmd):
 
     __classes = {
         "BaseModel",
-        "User"
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
     }
 
     def default(self, line):
